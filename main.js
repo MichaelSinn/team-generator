a5bda2c42552fd529e0b9ea22cb07fdfbadcc84c
MichaelSinn <michaelsinn.contact@gmail.com>
2022-10-12 20:36:15 -0400

    Commented code

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Import packages and classes
 const inquirer = require("inquirer");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
@@ -93,23 +94,23 @@ const team = {
     interns: []
 };
 
-function menu() {
+function menu() { // Menu function
     inquirer.prompt(menuPrompts).then(responses => { // Ask the prompts for the menu
-        if (responses["menuOptions"] === "Engineer") {
+        if (responses["menuOptions"] === "Engineer") { // If you choose to add an Engineer
             inquirer.prompt(engineerPrompts).then(responses => {
-                const {name, id, email, github} = responses;
-                team.engineers.push(new Engineer(name, id, email, github));
-                menu();
+                const {name, id, email, github} = responses; // Deconstruct the responses
+                team.engineers.push(new Engineer(name, id, email, github)); // Create a new engineer and add it to the team
+                menu(); // Go back to the menu
             });
-        } else if (responses["menuOptions"] === "Intern") {
+        } else if (responses["menuOptions"] === "Intern") { // If you choose to add an Intern
             inquirer.prompt(internPrompts).then(responses => {
-                const {name, id, email, school} = responses;
-                team.interns.push(new Intern(name, id, email, school));
-                menu();
+                const {name, id, email, school} = responses; // Deconstruct the responses
+                team.interns.push(new Intern(name, id, email, school)); // Create a new intern and add it to the team
+                menu(); // Go back to the menu
             });
-        } else if (responses["menuOptions"] === "Finish Building Team") {
-            generateHtml(team);
-            generateCss();
+        } else if (responses["menuOptions"] === "Finish Building Team") {  // If you choose to finish building your team
+            generateHtml(team); // Generate the HTML
+            generateCss(); // Generate the CSS
         }
     }).catch(e => {
         // Catch any errors
@@ -117,12 +118,12 @@ function menu() {
     });
 }
 
-function init() {
-    inquirer.prompt(managerPrompts).then(responses => {
-        const {name, id, email, officeNumber} = responses;
-        team.manager = new Manager(name, id, email, officeNumber);
-        menu()
+function init() { // Initialization function
+    inquirer.prompt(managerPrompts).then(responses => { // Create the manager
+        const {name, id, email, officeNumber} = responses; // Deconstruct the responses
+        team.manager = new Manager(name, id, email, officeNumber); // Add manager to the team
+        menu(); // Call the menu
     });
 }
 
-init();
+init(); // Call the initialization function
